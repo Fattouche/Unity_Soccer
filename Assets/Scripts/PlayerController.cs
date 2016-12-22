be54dd5ae66937be0f71007307cb26e6a59c18d2
Fattouche <Alex.fattouche@gmail.com>
2016-12-21 19:45:47 -0700

    Creation of map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider pickUpObject)
+    {
+        if (pickUpObject.gameObject.CompareTag("Pick Up Object"))
+        {
+            pickUpObject.gameObject.SetActive(false);
+        }
+           
+        //Destroy(pickUpObject.gameObject);
+    }
+
+
+}
